bd2a7e266e22ba58cf9dfcaf31c911192370311a
AlexandruChet <chetreanalexandru63@gmail.com>
2024-05-23 00:36:45 +0300

    ?

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ let score = 0;
 document.getElementById('clickerButton').addEventListener('click', () => {
     score++;
     document.getElementById('score').innerText = score;
+    
+    if (score > 50) {
+        document.querySelector('.container').style.backgroundImage = "url('https://th.bing.com/th/id/OIP.dA5d4bv2iUfWuPtI0yHobQAAAA?rs=1&pid=ImgDetMain')";
+    }
 });
+
