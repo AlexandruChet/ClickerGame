dfd7b18cf63aadcdc3fb0732b949e5b54842e638
AlexandruChet <chetreanalexandru63@gmail.com>
2024-05-26 02:08:52 +0300

    I write code at night I did the Update

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const dino  = document.getElementById("dino")
+const cactus = document.getElementById("cactus")
+
+document.addEventListener("keydown", function(event) {
+    jump();
+}); 
+
+
+function jump () {
+    if (dino.classList != "jump"){
+        dino.classList.add("jump")
+    }
+    setTimeout( function() {
+        dino.classList.remove("jump")
+    }, 300)
+}
+
 let score = 0;
 
 document.getElementById('clickerButton').addEventListener('click', () => {
